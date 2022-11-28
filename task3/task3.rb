eb95d2c49021481086ab13de49346b9a6a0c31a8
Shaan <shahrukhnawaz98@gmail.com>
2022-11-29 02:55:18 +0500

    completed needs cleaning

--- task3/task3.rb
+++ task3/task3.rb
@@ -13,24 +13,48 @@
 # # folder_id = '1v8kAzirygnGsKm4X0eX_OhNgFPw865aQ'
 
 require 'pdf-reader'
+require 'date'
 
 files = Dir['./files/*']
+results = []
+
+def get_name(string)
+    index = 0
+    name = []
+    while string[index] != 'Supreme'
+        name.append(string[index])
+        index+=1
+    end
+    return name.join(' ').split(',').join(',')
+end
+
+def get_date(string)
+    arr = string.split("/")
+    if arr[-1].length == 4
+        date = DateTime.parse("%s/%s/%s" % [arr[1],arr[0],arr[2]])
+    else
+        date = DateTime.parse("%s/%s/20%s" % [arr[0],arr[1],arr[2]])
+    end
+    return date.strftime('%Y/%m/%d')
+end
 
 files.each { |file| 
-
     reader = PDF::Reader.new(file)
-
     reader.pages.each do |page|
         text = page.text
-        puts text
-        # if text.include? 'Judgment for Costs of Appointed Attorney'
-        #     puts file
-        #     puts "yes"
-        # end
+        if text.include? 'Judgment for Costs'
+            s = text.split()
+            state = s[6..8].join(' ')
+            petitioner = get_name(s[9..])
+            date = get_date(s[s.find_index('Notice:')+1])
+            amount = s.select{|item| item.include?'$'}[0].split('.')[0]
+            results.append({
+                ":petitioner" => petitioner , 
+                ":state" => state,
+                ":amount"=> amount, 
+                ":date" => date
+            })
+        end
     end
-
 }
-
-
-
-
+puts results
